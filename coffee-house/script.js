29a784b1a42ec73b30ff058725fc4085175636bd
nestserka <nechatherina@gmail.com>
2023-11-27 15:48:44 +0900

    feat: add carusel animation

--- coffee-house/script.js
+++ coffee-house/script.js
@@ -6,7 +6,6 @@ const navLinks = document.querySelectorAll('.menu__link');
 // burger menu
 
 burger.addEventListener('click', function () {
-    console.log("click");
     this.classList.toggle('active');
     if (nav.classList.contains('open')) {
       closeMenu();
@@ -39,3 +38,60 @@ navLinks.forEach(link => {
 
 
 // carusel
+const btnPrev = document.querySelector('.left-arrow');
+const btnNext = document.querySelector('.right-arrow');
+const slider = document.querySelector('.slider-track__width');
+const sliderItems = document.querySelector('.slider-track');
+const items = document.querySelectorAll('.slider-container');
+const slidesToScroll = 1;
+const slidesToShow = 1;
+const itemsCount = items.length;
+let position = 0;
+const totalItems = document.querySelectorAll('.slider-container').length;
+const slideWidth = 100 / totalItems;
+sliderItems.style.width = `${totalItems * 100}%`;
+items.forEach(item => {
+    item.style.width = `${slideWidth}%`;
+});
+
+
+const moveToNextSlide = () => {
+  position -= slidesToScroll;
+  if (position < -(itemsCount - slidesToShow)) {
+      position = 0;
+  }
+  setPosition();
+};
+
+const moveToPrevSlide = () => {
+  position += slidesToScroll;
+  if (position > 0) {
+      position = -(itemsCount - slidesToShow);
+  }
+  setPosition();
+};
+
+const setPosition = () => {
+    sliderItems.style.transform = `translateX(${position * slideWidth}%)`;
+    updateRadioButtons(position);
+    window.clearInterval(mainInterval);
+    mainInterval = window.setInterval(moveToNextSlide, 5100);
+};
+
+btnNext.addEventListener('click', moveToNextSlide);
+btnPrev.addEventListener('click', moveToPrevSlide);
+
+
+
+const radioButtons = document.querySelectorAll('.favourite-pagination input[type="radio"]');
+
+const updateRadioButtons = (index) => {
+  radioButtons[Math.abs(index)].checked = true;
+  radioButtons.forEach((rb) => {
+    rb.disabled = rb.checked;
+  });
+};
+
+let mainInterval = setInterval(function(){moveToNextSlide();}, 5100);
+window.onload = mainInterval;
+
